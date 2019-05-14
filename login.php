f056ad8b7100f8b20d97ddd01de0c47f477d55ee
emup <emuppet@protonmail.com>
2019-05-14 15:24:53 +0200

    file created and connection to db

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+
+// Create connection
+$conn = new mysqli("localhost", "root", "", "cuddly-palm-tree");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
